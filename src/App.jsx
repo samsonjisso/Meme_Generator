5450965735df99396334ccdb8f928320f0af395f
samsonjisso <samsonjisso300@gmail.com>
2024-05-17 16:58:12 +0300

    day 4 @work

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavBar from "./component/NavBar";
+
+function App() {
+  return (
+    <main>
+    <NavBar />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,10 @@
+
+export default function NavBar() {
+    return (
+        <nav className="nav">
+            <img className="nav--logo" src="/assets/troll_face.png"/>
+        <h1 className="nav--title">Meme Generator</h1>
+        <h4 className="">React Course - Project 3</h4>
+        </nav>
+    )
+}
