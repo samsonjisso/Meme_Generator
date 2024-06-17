93cc071110132c0b9215c5c0fc86277b6507fbe1
samsonjisso <samsonjisso300@gmail.com>
2024-06-17 16:57:52 +0300

    form update

--- src/form/AppForm.jsx
+++ src/form/AppForm.jsx
@@ -1,25 +1,57 @@
 import { useState } from "react";
-import "./Form.css"
+import "./Form.css";
 
-export default function AppForm() { 
-    const [formData, setFormData] = useState({
-      firstName: "",
-      lastName: "",
-      email: ""
-    })
-    const handleChange = (event) => {
-      setFormData(prevFormData => {
-        return {
-          ...prevFormData,
-        [event.target.name]: event.target.value
-        }
-      })
-    }
+export default function AppForm() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comment: "",
+  });
+  const handleChange = (event) => {
+    console.log(event.target.name);
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
   return (
     <div className="form">
-      <input className="form--first" type="text" placeholder="First Name" onChange={handleChange} name="firstName"/>
-      <input className="form--last" type="text" placeholder="Last Name" onChange={handleChange} name="lastName"/>
-      <input className="form--email" type="text" placeholder="email" onChange={handleChange} name="email"/>
+      <input
+        className="form--first"
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <br />
+      <input
+        className="form--last"
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <br />
+      <input
+        className="form--email"
+        type="text"
+        placeholder="email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
+      />
+      <br />
+      <textarea
+        value={formData.comment}
+        placeholder="Comments"
+        onChange={handleChange}
+        name="comment"
+      />
     </div>
   );
 }
